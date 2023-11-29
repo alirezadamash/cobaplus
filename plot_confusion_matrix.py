4f2635d55c88bda115d5e7b4ce803ceedc606afb
Alireza Mansouri <alirezadamash@gmail.com>
2023-11-29 20:48:12 +0330

    update

--- plot_confusion_matrix.py
+++ plot_confusion_matrix.py
@@ -1,45 +1,28 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import confusion_matrix
-import ast
+import pandas as pd
 import numpy as np
-import re
+import os
+import matplotlib.pyplot as plt
+import ast
 
-# Load the data
-adam_data = pd.read_csv('result/MNIST/resnet/Adam_Existing_231125172257.csv')
-cobams_data = pd.read_csv('result/MNIST/resnet/CoBAMSGrad2_FR_231125193417.csv')
+# Load the csv files
+df1 = pd.read_csv('result/MNIST/resnet/Adam_Existing_231125172257.csv')
+df2 = pd.read_csv('result/MNIST/resnet/CoBAMSGrad2_FR_231125193417.csv')
 
-# Extract the confusion matrix for each epoch
-for cm in adam_data['test_confusion_matrix']:
-    processed_cm = '[' + re.sub(r'(\d)\s*', r'\1, ', cm) + ']'
-    print(processed_cm)
-    try:
-        ast.literal_eval(processed_cm)
-    except ValueError as e:
-        print(f"Error for {processed_cm}: {e}")
+# Create a directory to save the plots
+if not os.path.exists('Figures/confusion_matrix'):
+    os.makedirs('Figures/confusion_matrix')
 
-for cm in cobams_data['test_confusion_matrix']:
-    processed_cm = '[' + re.sub(r'(\d)\s*', r'\1, ', cm) + ']'
-    print(processed_cm)
-    try:
-        ast.literal_eval(processed_cm)
-    except ValueError as e:
-        print(f"Error for {processed_cm}: {e}")
+# Plot the heatmap of test_confusion_matrix for each epoch
+for i in range(len(df1)):
+    confusion_matrix1 = np.array(ast.literal_eval(df1['test_confusion_matrix'][i])).reshape(10, 10)
+    sns.heatmap(confusion_matrix1, annot=True, fmt='d')
+    plt.savefig(f'confusion_matrix/Adam_Existing_231125172257_epoch_{i}.png')
+    plt.close()
 
-print(adam_cm.shape)
+for i in range(len(df2)):
+    confusion_matrix2 = np.array(ast.literal_eval(df2['test_confusion_matrix'][i])).reshape(10, 10)
+    sns.heatmap(confusion_matrix2, annot=True, fmt='d')
+    plt.savefig(f'confusion_matrix/CoBAMSGrad2_FR_231125193417_epoch_{i}.png')
+    plt.close()
 
-# For each epoch, plot the confusion matrix
-for epoch in range(len(adam_cm)):
-    # Create a new figure
-    plt.figure()
-
-    # Plot the confusion matrix for Adam optimizer
-    sns.heatmap(adam_cm[epoch], annot=True, fmt='d')
-    plt.title('Adam Confusion Matrix for Epoch ' + str(epoch))
-    plt.show()
-
-    # Plot the confusion matrix for CoBAMSGrad2 optimizer
-    sns.heatmap(cobams_cm[epoch], annot=True, fmt='d')
-    plt.title('CoBAMSGrad2 Confusion Matrix for Epoch ' + str(epoch))
-    plt.show()
