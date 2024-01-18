d1075d9da7c6bdc65a6465ea6506e5faa0d0d047
Alireza Mansouri <alirezadamash@gmail.com>
2024-01-19 00:04:32 +0330

    Update

--- plot_aupr.py
+++ plot_aupr.py
@@ -6,8 +6,8 @@ cobaplus_data = pd.read_csv('result/MNIST/resnet/CoBAMSGrad2_PRP_231201215547.cs
 coba_data = pd.read_csv('result/MNIST/resnet/Adam_Existing_231201210940.csv')
 
 # Extract the f1 score every 100 epochs
-cobaplus_f1 = cobaplus_data['test_aupr']
-coba_f1 = coba_data['test_aupr']
+cobaplus_f1 = cobaplus_data['test_roc_auc']
+coba_f1 = coba_data['test_roc_auc']
 
 # Create a new figure
 plt.figure()
@@ -18,8 +18,8 @@ plt.plot(coba_f1, label='Adam')
 
 # Add labels and title
 plt.xlabel('Epochs')
-plt.ylabel('AUPR Score')
-plt.title('AUPR Score by Epoch')
+plt.ylabel('AUC/ROC Score')
+plt.title('AUC/ROC Score by Epoch')
 plt.legend()
 
 # Show the plot
--- plot_f1_all_clear.py
+++ plot_f1_all_clear.py
@@ -3,21 +3,37 @@ import matplotlib.pyplot as plt
 import os
 
 # Get all csv files in the directory
-csv_files = [f for f in os.listdir('result/MNIST/resnet/') if f.endswith('.csv')]
+csv_files = [f for f in os.listdir('result/MNIST/resnet/') if f.endswith('.csv') and 'const' not in f]
 
 # Create a new figure with a defined figure size for better clarity
 plt.figure(figsize=(10, 6))
 
-# Loop through all csv files and plot the f1 scores
+# Define a dictionary to map full names to short names
+name_mapping = {
+    'Adam_Existing': 'Adam',
+    'AdaGrad_Existing': 'AdaGrad',
+    'AMSGrad_Existing': 'AMSGrad',
+    'RMSProp_Existing': 'RMSProp',
+    'CoBAMSGrad2_FR': 'Proposed_FR',
+    'CoBAMSGrad2_CD': 'Proposed_CD',
+    'CoBAMSGrad2_HS': 'Proposed_HS',
+    'CoBAMSGrad2_LS': 'Proposed_LS',
+    'CoBAMSGrad2_PRP': 'Proposed_PRP',
+    'CoBAMSGrad2_DY': 'Proposed_DY'
+}
+
+# Loop through all csv files and plot the AUC ROC
 for csv_file in csv_files:
     data = pd.read_csv(f'result/MNIST/resnet/{csv_file}')
-    f1_scores = data['test_f1']
-    plt.plot(f1_scores, label=csv_file.split('.')[0])
+    AUC_ROC = data['test_roc_auc']
+    # Get the short name from the dictionary using the csv file name
+    short_name = name_mapping.get('_'.join(csv_file.split('_')[:-1]), csv_file.split('.')[0])
+    plt.plot(AUC_ROC, label=short_name)
 
 # Add labels and title with larger font sizes
 plt.xlabel('Epochs', fontsize=14)
-plt.ylabel('F1 Score', fontsize=14)
-plt.title('F1 Score by Epoch', fontsize=16)
+plt.ylabel('AUC/ROC', fontsize=14)
+plt.title('AUC/ROC by Epoch', fontsize=16)
 plt.legend(fontsize=12)
 
 # Show the plot with a grid for better clarity
